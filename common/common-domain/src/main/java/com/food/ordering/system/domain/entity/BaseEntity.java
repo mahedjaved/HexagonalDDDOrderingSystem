fd12d240a8767b5615ad234ddcfd5334872d1e4b
mahedjaved <mahed95@gmail.com>
2024-08-29 18:54:14 +0100

    adds baseEntity and AggregateRoot abstract classes in the common module

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
@@ -1,5 +1,9 @@
 package com.food.ordering.system.domain.entity;
 
+// reason why we have "common" module is because alot the entities that domain objects have are similar, these can be defined independantly here
+// hence have this as an ABSTRACT class
+// doing this also allows easy differentiation of Entity Vs Aggregate Vs Aggregate Root, since these types will expose which abstract class they are implementing
+
 import java.util.Objects;
 
 public abstract class BaseEntity<ID> {
@@ -13,6 +17,7 @@ public abstract class BaseEntity<ID> {
         this.id = id;
     }
 
+    //    Below are "contracts" used to compare two Java objects
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
