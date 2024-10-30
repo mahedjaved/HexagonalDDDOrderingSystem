d5404bae0d6efbfb034e1455c44918a1298645a8
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-10-30 04:26:42 +0000

    updates OrderStatus and adds ProductId, CustomerId and RestaurantId

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/valueobjects/CustomerId.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.domain.entity.valueobjects;
+
+public class CustomerId {
+}
---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/valueobjects/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.food.ordering.system.domain.entity.valueobjects;
+
+public enum OrderStatus {
+    PENDING, PAID, APPROVED, CANCELLED, CANCELLING
+}
