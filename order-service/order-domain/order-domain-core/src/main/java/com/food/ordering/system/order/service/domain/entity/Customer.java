a316df53833c18b76a3f48c3b41fb48aa300a8b6
mahedjaved <mahed95@gmail.com>
2025-01-09 14:26:33 +0000

    - Adds Restaurant.java and Customer.java entities to the order service module

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,6 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.entity.AggregateRoot;
+
+public class Customer extends AggregateRoot<Customer> {
+}
